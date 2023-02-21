8cadcefd97fab0fc1d1e293e853906c70c591e01
braplies <kennedykissi0411@gmail.com>
2023-02-21 11:14:17 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	char Kennedy[] = "Kennedy";
-	int i = 0;
+char Kennedy[] = "Kennedy";
+int i = 0;
 
-	while (Kennedy[i] != '\0')
-	{
-		_putchar(Kennedy[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+while (Kennedy[i] != '\0')
+{
+_putchar(Kennedy[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
