03f1d11704fec517a34adf0d9a0c77eff0c93975
braplies <kennedykissi0411@gmail.com>
2023-02-21 10:55:46 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char Kennedy[] = "Kennedy";
+	int i = 0;
+
+	while (Kennedy[i] != '\0')
+	{
+		_putchar(Kennedy[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
