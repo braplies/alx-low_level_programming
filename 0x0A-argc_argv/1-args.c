03db9edae9ca417d84b7e4c07d7627ca3b8debb1
braplies <kennedykissi0411@gmail.com>
2023-03-10 15:47:18 -0800

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,37 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins
+ * main - prints the number of arguments passed into it
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int cents, ncoins = 0;
-
-	if (argc == 1 || argc > 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	cents = atoi(argv[1]);
-	while (cents > 0)
-	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
-	}
-	printf("%d\n", ncoins);
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
